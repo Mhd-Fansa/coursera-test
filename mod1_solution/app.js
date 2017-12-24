be8e52747141798592e6bc0b9903fd59aea02b59
Mhd Fansa <m.fansa92@gmail.com>
2017-12-24 20:53:03 +0300

    'mod1_solution_Final'

---
+++ mod1_solution/app.js
@@ -0,0 +1,35 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+    
+LunchCheckController.$inject = [ '$scope' ];
+
+function LunchCheckController($scope) {
+    
+    $scope.check = function() {
+        if (!$scope.lunchItems) {
+				$scope.message = 'Please enter data first';
+			} else {
+				$scope.lunchItems = $scope.lunchItems.split(',');
+				var items = [];
+                
+                for (var i = 0; i < $scope.lunchItems.length; i++){
+					if($scope.lunchItems[i])items.push($scope.lunchItems[i]);
+				}
+                $scope.lunchItems = items;
+				if($scope.lunchItems.length === 0){
+					$scope.message = 'Please enter data first';
+					return;
+				}
+				if ($scope.lunchItems.length <= 3) {
+					$scope.message = 'Enjoy!';
+				} else {
+					$scope.message = 'Too much!!';
+				}
+			}
+			$scope.lunchItems = undefined;
+		};
+    }
+})();
